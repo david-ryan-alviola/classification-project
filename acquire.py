ad1d382aab68bc67ce6944bf5d5a93b2853c3088
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-03-05 22:39:49 -0600

    Added acquire.py. Added env.py.dist as template. Updated .gitignore file.

---
+++ acquire.py
@@ -0,0 +1,17 @@
+import sys
+
+from env import user, password, host, util_path
+sys.path.append(util_path) # Only required if utilities.py is not in your project directory
+from utilities import generate_db_url, generate_df
+
+def acquire_telco_churn_data():
+    
+    telco_query = """
+    SELECT *
+        FROM customers
+            JOIN contract_types USING(contract_type_id)
+            JOIN internet_service_types USING(internet_service_type_id)
+            JOIN payment_types USING(payment_type_id);
+    """
+
+    return generate_df("telco_churn.csv", telco_query, generate_db_url(user, password, host, "telco_churn"))
