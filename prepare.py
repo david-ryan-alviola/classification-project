548de9316092a455d3fb2eee9d59f058fa33cb21
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-03-08 10:46:39 -0600

    Updated .gitignore and prepare.py

--- prepare.py
+++ prepare.py
@@ -12,40 +12,32 @@ def prepare_telco_data(telco_df):
     telco_df.total_charges.fillna(value=telco_df.monthly_charges, inplace=True)
 
     # These columns are redundant or provide no useful information
-    drop_columns = ['Unnamed: 0', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id']
+    drop_columns = ['Unnamed: 0', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id', 'multiple_lines']
     telco_df.drop(columns=drop_columns, inplace=True)
-
-    # Replace No internet service or No phone service to No since they are pretty much the same
-    telco_df.replace(to_replace="No phone service", value="No", inplace=True)
-    telco_df.replace(to_replace="No internet service", value="No", inplace=True)
 
     # Convert total_charges to float
     telco_df.total_charges = telco_df.total_charges.astype(float)
+    
+    # Simplify related columns by combining them
+    telco_df['internet_extras'] = (telco_df.online_backup == "Yes") | (telco_df.online_security == "Yes") | (telco_df.device_protection == "Yes") | (telco_df.tech_support == "Yes")
+    telco_df['streaming_entertainment'] = (telco_df.streaming_movies == "Yes") | (telco_df.streaming_tv == "Yes")
+    telco_df['family'] = (telco_df.partner == "Yes") | (telco_df.dependents == "Yes")
+
+    # Drop combined columns
+    telco_df.drop(columns=['online_backup', 'online_security', 'device_protection', 'tech_support', 'streaming_movies', 'streaming_tv', 'partner', 'dependents'], inplace=True)
 
     # These columns need to be encoded
-    encode_dict = {'gender' : True,
-                'partner' : True,
-                'dependents' : True,
-                'phone_service' : True,
-                'multiple_lines' : True,
-                'online_security' : True,
-                'online_backup' : True,
-                'device_protection' : True,
-                'tech_support' : True,
-                'streaming_tv' : True,
-                'streaming_movies' : True,
-                'paperless_billing' : True,
-                'churn' : True,
-                'contract_type' : False,
-                'internet_service_type' : False,
-                'payment_type' : False
-                }
-    dummies_df = pd.get_dummies(telco_df[list(encode_dict.keys())], dummy_na=False, drop_first=list(encode_dict.values()))
+    drop_first_true = ['gender', 'phone_service', 'paperless_billing', 'churn']
+    drop_first_false = ['contract_type', 'internet_service_type', 'payment_type']
+
+    dummies_df1 = pd.get_dummies(telco_df[drop_first_true], dummy_na=False, drop_first=True)
+    dummies_df2 = pd.get_dummies(telco_df[drop_first_false], dummy_na=False, drop_first=False)
 
     # Merge dummies with telco_df
-    telco_df = pd.concat([telco_df, dummies_df], axis=1)
+    telco_df = pd.concat([telco_df, dummies_df1, dummies_df2], axis=1)
 
     # Drop columns that are now encoded
-    telco_df.drop(columns=list(encode_dict.keys()), inplace=True)
+    telco_df.drop(columns=drop_first_false, inplace=True)
+    telco_df.drop(columns=drop_first_true, inplace=True)
 
     return split_dataframe(telco_df, stratify_by="churn_Yes")
